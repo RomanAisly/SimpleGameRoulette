bc81e348a8d0656ab9c89e276e73c6d3dd147087
Roman Aisly <romanaislydevelop@gmail.com>
2023-07-25 19:01:49 +0300

    first commit

---
+++ app/src/main/java/com/example/simplegameroulette/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.simplegameroulette.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+val red = Color(0xFFFF1100)
+val blue = Color(0xFF0027FF)
+val green = Color(0xFF00FF0A)
+val yellow = Color(0xFFFFE500)
+val purple = Color(0xFFDA00FF)
